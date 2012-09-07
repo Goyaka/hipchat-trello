c167428b1aa66223f82a6ad5f417d2f06ce33244
Alagu <allagappan@gmail.com>
2012-09-07 19:32:28 +0000

    Scripts

--- script.sh
+++ script.sh
@@ -2,5 +2,9 @@
 # 
 # #crontab -e
 # */10 * * * * bash /home/ubuntu/markupwand-jobs/grove-trello/script.sh
+export PATH=/usr/local/rvm/bin:$PATH
+. /usr/local/rvm/scripts/rvm
+rvm use 1.9.2-p320
 cd /home/ubuntu/markupwand-jobs/grove-trello/
+ruby --version
 ruby fetch_once.rb
